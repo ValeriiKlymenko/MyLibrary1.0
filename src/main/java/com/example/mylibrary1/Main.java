e7177e7b88074d9ee791076f9e1908433626a480
KlimJavaDev <klimjavadev@gmail.com>
2022-07-05 17:35:00 +0300

    Update version project

--- src/main/java/com/example/mylibrary1/Main.java
+++ src/main/java/com/example/mylibrary1/Main.java
@@ -7,13 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-regist-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-regist-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 400);
-        stage.setTitle("My Library v1.0");
+        stage.setTitle("My Library v2.0");
         stage.setScene(scene);
         stage.show();
     }
